bc3c4a1fc6d690db13d980185109353549842afe
Alexander Martin <76964530+alexmartin1722@users.noreply.github.com>
2022-07-12 11:02:54 -0400

    Update householderQR.m
    Still temp fixed, just doesnt produce output

--- householderQR.m
+++ householderQR.m
@@ -20,14 +20,14 @@ R = X;
 for i = 1:n
     z = R(i,i)-(-sign(R(i,i))*norm(R(i:end,i)));
     v = R(i:end,i)/z;
-    v(1) = 1
-    p = (sign(R(i,i))*z)/norm(R(i:end,i))
+    v(1) = 1;
+    p = (sign(R(i,i))*z)/norm(R(i:end,i));
 
     R(i:end,:) = R(i:end,:)-(p*v)*(v'*R(i:end,:));
     Q(:,i:end) = Q(:,i:end)-(Q(:,i:end)*v)*(p*v)';
 end
-Q = Q*-1
-R = triu(R) * -1
+Q = Q * -1;
+R = triu(R) * -1;
 
 
     
