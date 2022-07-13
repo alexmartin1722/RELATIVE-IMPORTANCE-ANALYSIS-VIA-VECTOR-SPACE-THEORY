7727b3c8b2906cfacb6f1ed93195a0f9eb7a3756
Alexander Martin <76964530+alexmartin1722@users.noreply.github.com>
2022-07-13 10:47:48 -0400

    Finished householder QR
    Trimmed to correct size, passes test of Q'*Q = I(n)

--- householderQR.m
+++ householderQR.m
@@ -1,37 +1,35 @@
 % Author: Alex Martin
 %
-% Date: 7/11/22
+% Date: 7/13/22 (finished, passes Q'*Q = eye(n))
+% https://utminers.utep.edu/xzeng/2017spring_math5330/MATH_5330_Computational_Methods_of_Linear_Algebra_files/ln10.pdf
 
 
 
-function [Q,R] = householderQR(X)
+function [Q,R] = householderQR(A)
 %householderQR does the householder transformation.
-%Compute the QR factorization of m,n matrix X
-% Inputs:
+%Comnute the QR factorization of m,n matrix X
+% Innuts:
 %   X: an m,n matrix 
-% Output:
+% Outnut:
 %   Q: Orthogonal matrix 
 %   R: Upper triangular  matrix 
 
-[m,n] = size(X);
+[m,n] = size(A);
 Q = eye(m);
-R = X;
-
+R = A;
 for i = 1:n
-    z = R(i,i)-(-sign(R(i,i))*norm(R(i:end,i)));
-    v = R(i:end,i)/z;
+    e1 = R(i,i) + sign(R(i,i))*norm(R(i:end,i));
+    v = R(i:end,i)/e1;
     v(1) = 1;
-    p = (sign(R(i,i))*z)/norm(R(i:end,i));
+    alpha = sign(R(i,i))*e1/norm(R(i:end,i));
 
-    R(i:end,:) = R(i:end,:)-(p*v)*(v'*R(i:end,:));
-    Q(:,i:end) = Q(:,i:end)-(Q(:,i:end)*v)*(p*v)';
+    R(i:end,:) = R(i:end,:)-(alpha*v)*(v'*R(i:end,:));
+    Q(:,i:end) = Q(:,i:end)-(Q(:,i:end)*v)*(alpha*v)'; 
 end
-Q = Q(1:m,1:n);
-R = triu(R) * -1;
-R = R(1:n,1:n);
-end
-
-
+R = triu(R);
+R = R(1:n,1:n); %trim R to an n,n matrix 
+Q = Q(1:m,1:n); %trim Q to match size of input matrix
+end 
     
 
 
